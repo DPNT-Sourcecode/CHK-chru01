8067372d1fd4e8fabd95551e976e19522edb005a
sbx_user1051 <sbx_user1051@169.254.19.53>
2023-05-25 14:40:40 +0000

    Thu May 25 14:40:40 UTC 2023

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -6,5 +6,10 @@ class TestSum():
         assert checkout_solution.checkout("ABCL") == -1
         assert checkout_solution.checkout("ABCD") == 115
         assert checkout_solution.checkout("AAAA") == 180
+        assert checkout_solution.checkout("AAAAA") == 200
+        assert checkout_solution.checkout("AAAAAA") == 250
+        assert checkout_solution.checkout("AAAAAAAA") == 330
         assert checkout_solution.checkout("BBBB") == 90
         assert checkout_solution.checkout("AAABBABCD") == 130 + 45 + 115
+        assert checkout_solution.checkout("BBBBBEEEE") == 75 + 160
+
