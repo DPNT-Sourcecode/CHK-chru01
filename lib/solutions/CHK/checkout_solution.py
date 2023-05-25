26d1b0e505b4f856048332e89757c535cebf29ce
sbx_user1051 <sbx_user1051@169.254.19.53>
2023-05-25 14:51:31 +0000

    Thu May 25 14:51:31 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -11,24 +11,24 @@ PRICES = {
     "F": 10,
     "G": 20,
     "H": 10,
-I    | 35    |                        |
-| J    | 60    |                        |
-| K    | 80    | 2K for 150             |
-| L    | 90    |                        |
-| M    | 15    |                        |
-| N    | 40    | 3N get one M free      |
-| O    | 10    |                        |
-| P    | 50    | 5P for 200             |
-| Q    | 30    | 3Q for 80              |
-| R    | 50    | 3R get one Q free      |
-| S    | 30    |                        |
-| T    | 20    |                        |
-| U    | 40    | 3U get one U free      |
-| V    | 50    | 2V for 90, 3V for 130  |
-| W    | 20    |                        |
-| X    | 90    |                        |
-| Y    | 10    |                        |
-| Z    | 50    |       
+    "I": 35,    |                        |
+    "J": 60,    |                        |
+    "K": 80,    | 2K for 150             |
+    "L": 90,    |                        |
+    "M": 15,    |                        |
+    "N": 40,    | 3N get one M free      |
+    "O": 10,    |                        |
+    "P": 50,    | 5P for 200             |
+    "Q": 30,    | 3Q for 80              |
+    "R": 50,    | 3R get one Q free      |
+    "S": 30,    |                        |
+    "T": 20,    |                        |
+    "U": 40,    | 3U get one U free      |
+    "V": 50,    | 2V for 90, 3V for 130  |
+    "W": 20,    |                        |
+    "X": 90,    |                        |
+    "Y": 10,    |                        |
+    "Z": 50,    |                        |
     
 }
 
@@ -36,10 +36,15 @@ OFFERS_MULTIPLE = {
     "A": [(5, 200), (3, 130)],
     "B": [(2, 45)],
     "F": [(3, 20)],
+    "K": [(2, 50)],
+    "P": [(5, 200)],
+    "Q": [(3, 80)],
 }
 
 OFFERS_GET_FREE = {
     "E": (2, "B"),
+    "N": (3, "M"),
+    "R": (3, "Q"),
 }
 
 
@@ -78,4 +83,5 @@ def checkout(skus):
             res += basket[product] * PRICES[product]
             
     return res
+
 
