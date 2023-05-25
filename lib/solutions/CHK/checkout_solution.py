b6589a49e07dd095d0e1a8e97d38eef846c73e65
sbx_user1051 <sbx_user1051@169.254.19.53>
2023-05-25 15:17:09 +0000

    Thu May 25 15:17:09 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -68,15 +68,21 @@ def checkout(skus):
     
     res = 0
     
-    
-    
     for product in basket:
         if product in OFFERS_GET_FREE:
             temp_count = basket[product]
             temp_free_items = temp_count // OFFERS_GET_FREE[product][0]
             if OFFERS_GET_FREE[product][1] in basket:
-                basket[OFFERS_GET_FREE[product][1]] -= min(basket[OFFERS_GET_FREE[product][1]], temp_free_items)         
+                basket[OFFERS_GET_FREE[product][1]] -= min(basket[OFFERS_GET_FREE[product][1]], temp_free_items)    
+    
+    
+    for offer in OFFERS_GROUP:
+        temp_count = 0
+        for product in offer:
+            if product in basket:
+                bas
             
+    
     
     for product in basket:
             
@@ -91,5 +97,6 @@ def checkout(skus):
             res += basket[product] * PRICES[product]
             
     return res
+
 
 
